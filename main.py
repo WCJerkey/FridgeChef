28140667631b7a70bd33b81c541caa25cfd575a9
Enkhbaatar Saranbaatar <professor0913@yahoo.com>
2025-02-24 13:26:10 -0500

    First Enky Commit

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+#Root of the App
+window = tk.Tk()
+window.title('FridgeChef')
+window.geometry("600x600")
+window.minsize(600, 600)
+
+#Styling ttk widgets
+style = ttk.Style()
+style.theme_use('classic') # Any style other than aqua.
+
+style.configure("new.TFrame", background='#022b5f')
+
+base = ttk.Frame(window)
+base.pack(expand=True,fill='both')
+
+
+frame1 = ttk.Frame(base,padding=8, style="new.TFrame")
+frame2 = ttk.Frame(base)
+
+frame1.place(x=0, y = 0, relwidth=0.4, relheight=1)
+frame2.place(relx=0.4, y=0, relwidth=0.6, relheight=1)
+ttk.Label(frame1, text="Hello", background= 'red').pack(expand=True, fill = 'both')
+ttk.Label(frame2, text="bye", background= 'blue').pack(expand=True, fill = 'both')
+
+
+#The program runs here
+window.mainloop()
